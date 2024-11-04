b347c8c1c87c9f4b4ec109bcee66423ea191a667
RutaHeidemane <rutaheidemane@gmail.com>
2024-11-04 13:13:05 +0200

    Add files via upload

---
+++ PD01.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  4 12:12:34 2024
+
+@author: 6vsk.viesis1
+"""
+
+# %% 1
+
+x=int(input("Enterx:"))
+y=int(input("Enterx:"))
+print(float(x))
+print(str(y))
+
+# %% 2
+
+
+
+k=int(input("Enterx:"))
+l=int(input("Enterx:"))
+
+print ( k+l )
+print ( k-l )
+print ( k/l )
+print ( k*l )
+print ( k**l )
+print ( k//l )
+
+# %% 3
+
+x=int(input("Enterx:"))
+if x>0:
+    print("x ir pozitivs")
+else x<0
+    print("x ir negativs")
+else:
+    print("x ir 0")
+
+# %% 4
+
+x=1
+y=2
+x=int(input("Enterx:"))
+result= 4*x**y+3+15*y
+print(result)
+
+
+
+x=1
+y=2
+x=int(input("Enterx:"))
+result= 5*y**x-144*x+2/(x+y)**2
+print(result)
+
+
+x=1
+y=2
+x=int(input("Enterx:"))
+result=2+x-x*y/(y)/(x+y)
+print(result)
+
+
+# %% 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
